4c336ee28fd23e40ff00dc2e8931aeca4bdfb3e7
Gilberto Suarez <suarezgilberto@hotmail.com>
2020-07-16 11:22:29 -0400

    chore: footer add classname

--- src/components/presentational/Header/Header.js
+++ src/components/presentational/Header/Header.js
@@ -1,21 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
+import classNames from "classnames/bind";
+
 import Navbar from "components/presentational/Navbar";
 import SocialMediaBar from "components/presentational/SocialMediaBar";
 
 import styles from "./Header.module.scss";
 
+const classnames = classNames.bind(styles);
+
 Header.propTypes = {};
 
-function Header(props) {
+function Header({ className }) {
+  const containerClass = classnames(className, "header__container");
   return (
-    <header className={styles.header__container}>
+    <header className={containerClass}>
       <section className={styles["header__upper-section"]}>
-        <div>
+        <div className={styles.header__title}>
           <span className={styles["header__text-logo"]}>{"<GS/>"}</span>
           <span className={styles.header__text}>gilbertosuarez.com</span>{" "}
         </div>
-        <SocialMediaBar />
+        <SocialMediaBar className={styles["header__social-media-bar"]} />
       </section>
       <Navbar />
     </header>

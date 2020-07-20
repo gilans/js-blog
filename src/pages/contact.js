fb5d2ef92a35c49af496dfe8c6623dd514b8b982
Gilberto Suarez <suarezgilberto@hotmail.com>
2020-07-19 21:40:00 -0400

    feat: add contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+import MainLayout from "components/layout/MainLayout";
+
+contact.propTypes = {};
+
+function contact(props) {
+  return (
+    <MainLayout>
+      <p>Contact </p>
+    </MainLayout>
+  );
+}
+
+export default contact;
